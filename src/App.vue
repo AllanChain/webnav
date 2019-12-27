84fbdaae312961a7a6c0d6a3e6771b1a3c650a2d
Allan Chain <allanchain@pku.edu.cn>
2019-12-27 21:44:03 +0800

    first vue cli app

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <div class="logo" style="font-size: 100px; color: #fff; margin-bottom: 20px; line-height: 0">
+      4&emsp;<img src="./assets/octocat.svg" />&emsp;4
+    </div>
+
+    <WebNav />
+  </div>
+</template>
+
+<script>
+import WebNav from "./components/WebNav.vue";
+
+export default {
+  name: "app",
+  components: {
+    WebNav
+  }
+};
+</script>
+
+<style>
+#app {
+  margin-top: 30px;
+  text-align: center;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.logo {
+  max-width: 300px;
+  margin: auto;
+  animation: flipInX 3s;
+  -webkit-animation: flipInX 3s infinite;
+}
+
+.icon {
+  width: 2em;
+  height: 2em;
+  max-width: 65px;
+}
+
+@keyframes flipInX {
+  0% {
+    -webkit-transform: perspective(400px) rotateX(90deg);
+    -ms-transform: perspective(400px) rotateX(90deg);
+    transform: perspective(400px) rotateX(90deg);
+    opacity: 0;
+  }
+
+  40% {
+    -webkit-transform: perspective(400px) rotateX(-20deg);
+    -ms-transform: perspective(400px) rotateX(-20deg);
+    transform: perspective(400px) rotateX(-20deg);
+  }
+
+  55% {
+    -webkit-transform: perspective(400px) rotateX(25deg);
+    -ms-transform: perspective(400px) rotateX(25deg);
+    transform: perspective(400px) rotateX(25deg);
+  }
+
+  80% {
+    -webkit-transform: perspective(400px) rotateX(-30deg);
+    -ms-transform: perspective(400px) rotateX(-30deg);
+    transform: perspective(400px) rotateX(-30deg);
+    opacity: 1;
+  }
+  100% {
+    -webkit-transform: perspective(400px) rotateX(90deg);
+    -ms-transform: perspective(400px) rotateX(90deg);
+    transform: perspective(400px) rotateX(90deg);
+    opacity: 0;
+  }
+}
+</style>
---
+++ src/components/WebNav.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <div class="search">
+      <input v-model="query" type="text" id="search_input" />
+    </div>
+    <br />
+    <div
+      class="box"
+      v-for="(bookmark, index) in bookmarks"
+      :key="index"
+      :data-url="bookmark.url"
+      :data-search="bookmark.search"
+      @click="go"
+    >
+      <img class="icon" :src="bookmark.url_icon" />
+      <p class="url">{{ bookmark.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      bookmarks: null,
+      query: ""
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    go(event) {
+      let targetData = event.currentTarget.dataset;
+      let url;
+      if (this.query && targetData.search) {
+        url = targetData.url + "/" + targetData.search + this.query;
+      } else {
+        url = targetData.url;
+      }
+      window.location.href = url;
+    },
+    fetchData() {
+      axios
+        .get("/Bookmarks.json")
+        .then(response => {
+          this.bookmarks = response.data;
+          console.log(response);
+        })
+        .catch(error => console.log(error));
+    }
+  }
+};
+</script>
+
+<style>
+.box {
+  position: relative;
+  display: inline-block;
+  width: 65px;
+  border: 0;
+}
+
+#search_input {
+  padding: 5px 15px;
+  font-weight: 500;
+  font-size: 1em;
+  width: 70%;
+  max-width: 500px;
+  border-radius: 20px;
+  background-color: rgba(200, 200, 200, 0);
+  color: #333;
+  border: none;
+  box-shadow: 0 0 10px rgb(175, 23, 245);
+}
+
+input:focus {
+  outline: none;
+}
+
+.url {
+  color: #eee;
+  text-shadow: 1px 1px 3px #000;
+  height: 2em;
+  line-height: 1.1em;
+  width: 65px;
+  font-size: 0.75em;
+  white-space: wrap;
+  overflow: hidden;
+  margin: auto;
+  -webkit-border-top-right-radius: 5px;
+  -webkit-border-bottom-right-radius: 5px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  text-overflow: ellipsis;
+  -o-text-overflow: ellipsis;
+  -ms-text-overflow: ellipsis;
+  padding-top: 3px;
+  padding-bottom: 8px;
+}
+</style>
