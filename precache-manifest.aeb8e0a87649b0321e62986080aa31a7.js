937d75e578d7e9edfddadb9716ccfd197450edcd
Allan Chain <allanchain@pku.edu.cn>
2020-01-10 17:52:02 +0800

    f*****g manual update

---
+++ precache-manifest.aeb8e0a87649b0321e62986080aa31a7.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a94cae4b8754b3f478af1c83052e6b",
+    "url": "/webnav/Bookmarks.json"
+  },
+  {
+    "revision": "0db6f19cb1b51b736e9ef2f4a4a61580",
+    "url": "/webnav/back.jpg"
+  },
+  {
+    "revision": "3ac95939b71d930008fc",
+    "url": "/webnav/css/app.411f05a7.css"
+  },
+  {
+    "revision": "e415cb87be5ad670191a37d3d44686e4",
+    "url": "/webnav/icon/Allan Chain.ico"
+  },
+  {
+    "revision": "9fb559a691078558e77d6848202f6541",
+    "url": "/webnav/icon/Bing.ico"
+  },
+  {
+    "revision": "7f969f62ee272a3be19966806fff4ad5",
+    "url": "/webnav/icon/GitHub.ico"
+  },
+  {
+    "revision": "f3418a443e7d841097c714d69ec4bcb8",
+    "url": "/webnav/icon/Google.ico"
+  },
+  {
+    "revision": "c120ba1c67bb7e4e419e3cfcc7dd21ac",
+    "url": "/webnav/icon/Pypi.ico"
+  },
+  {
+    "revision": "54027b8023a0b674ee73be743dc1a61a",
+    "url": "/webnav/icon/P大树洞.ico"
+  },
+  {
+    "revision": "a6526d8765092944a4840ce8fb10b103",
+    "url": "/webnav/icon/Stack Overflow.ico"
+  },
+  {
+    "revision": "b007aeb0dede5cdcfe5d42c6be45dde6",
+    "url": "/webnav/icon/TG.ico"
+  },
+  {
+    "revision": "8814cf5f310dcc1e43801334c5118e0f",
+    "url": "/webnav/icon/Wolfram.ico"
+  },
+  {
+    "revision": "7839ff03ebbf84964ccc3ffd51b0f38c",
+    "url": "/webnav/icon/default.ico"
+  },
+  {
+    "revision": "60d44b6c28657bc83c6237f16cbde485",
+    "url": "/webnav/icon/handsome-ml.ico"
+  },
+  {
+    "revision": "5e7b32e7bfba7ac135891fcbbe962d3d",
+    "url": "/webnav/icon/北大网关.ico"
+  },
+  {
+    "revision": "82d5293924029f60de843d69015c5153",
+    "url": "/webnav/icon/北大门户.ico"
+  },
+  {
+    "revision": "cb87198c61c54b64dea18673576b76fd",
+    "url": "/webnav/icon/古诗文网.ico"
+  },
+  {
+    "revision": "cbffea0f28252914fe79e87ed981d00d",
+    "url": "/webnav/icon/未名BBS.ico"
+  },
+  {
+    "revision": "7839ff03ebbf84964ccc3ffd51b0f38c",
+    "url": "/webnav/icon/油猴.ico"
+  },
+  {
+    "revision": "b7dee4e8385ebb1b10d723807f097905",
+    "url": "/webnav/icon/浙江考试.ico"
+  },
+  {
+    "revision": "2d559756cd269d528cde01be6e259637",
+    "url": "/webnav/icon/温州台风.ico"
+  },
+  {
+    "revision": "587c37488cb060f2dffa18bc0cc9a684",
+    "url": "/webnav/icon/百度.ico"
+  },
+  {
+    "revision": "7839ff03ebbf84964ccc3ffd51b0f38c",
+    "url": "/webnav/icon/神经网络.ico"
+  },
+  {
+    "revision": "95bd79c3c8eb38497bc6cff3c0f2039a",
+    "url": "/webnav/icon/观察者.ico"
+  },
+  {
+    "revision": "a388abc14ce81ab733ae69a804e87880",
+    "url": "/webnav/icon/谷歌翻译.ico"
+  },
+  {
+    "revision": "4ee796780dcc1d39db947a636e4e87c4",
+    "url": "/webnav/img/octocat.4ee79678.svg"
+  },
+  {
+    "revision": "75d9c070e0325327a8e42f29b2869985",
+    "url": "/webnav/index.html"
+  },
+  {
+    "revision": "3ac95939b71d930008fc",
+    "url": "/webnav/js/app.cc038d30.js"
+  },
+  {
+    "revision": "e8cf6dbdb10eded4e7fd",
+    "url": "/webnav/js/chunk-vendors.3b182660.js"
+  },
+  {
+    "revision": "1eb0b18be1f4192d245f6635fd589497",
+    "url": "/webnav/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/webnav/robots.txt"
+  }
+]);
