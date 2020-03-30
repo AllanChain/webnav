77057e4e957e9346f75e9e5c006a75d38d10368b
Allan Chain <allanchain@pku.edu.cn>
2020-03-30 16:36:36 +0800

    Refractor: split logo into a component

--- src/App.vue
+++ src/App.vue
@@ -35,9 +35,7 @@
             <v-list-item-title class="title">
               WebNav
             </v-list-item-title>
-            <div class="logo text-center">
-              4&emsp;<img src="./assets/octocat.svg" width="50">&emsp;4
-            </div>
+            <logo class="text-center" />
           </v-list-item-content>
         </v-list-item>
         <v-divider />
@@ -72,14 +70,16 @@
 </template>
 
 <script>
-import JsonImport from "@/components/JsonImport";
-import WebNav from "@/components/WebNav";
+import JsonImport from "@/components/JsonImport"
+import WebNav from "@/components/WebNav"
+import Logo from '@/components/Logo'
 
 export default {
   name: "App",
   components: {
     JsonImport,
-    WebNav
+    WebNav,
+    Logo
   },
   data() {
     return {
@@ -113,45 +113,5 @@ export default {
 </script>
 
 <style>
-.logo {
-  font-size: 65px;
-  color: #2196f3;
-  animation: flipInX 3s;
-  -webkit-animation: flipInX 3s infinite;
-  line-height: 0;
-}
 
-@keyframes flipInX {
-  0% {
-    -webkit-transform: perspective(400px) rotateX(90deg);
-    -ms-transform: perspective(400px) rotateX(90deg);
-    transform: perspective(400px) rotateX(90deg);
-    opacity: 0;
-  }
-
-  40% {
-    -webkit-transform: perspective(400px) rotateX(-20deg);
-    -ms-transform: perspective(400px) rotateX(-20deg);
-    transform: perspective(400px) rotateX(-20deg);
-  }
-
-  55% {
-    -webkit-transform: perspective(400px) rotateX(25deg);
-    -ms-transform: perspective(400px) rotateX(25deg);
-    transform: perspective(400px) rotateX(25deg);
-  }
-
-  80% {
-    -webkit-transform: perspective(400px) rotateX(-30deg);
-    -ms-transform: perspective(400px) rotateX(-30deg);
-    transform: perspective(400px) rotateX(-30deg);
-    opacity: 1;
-  }
-  100% {
-    -webkit-transform: perspective(400px) rotateX(90deg);
-    -ms-transform: perspective(400px) rotateX(90deg);
-    transform: perspective(400px) rotateX(90deg);
-    opacity: 0;
-  }
-}
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="logo py-4">
+    4&emsp;<img src="@/assets/octocat.svg" width="50">&emsp;4
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.logo {
+  font-size: 65px;
+  color: #2196f3;
+  animation: flipInX 3s;
+  -webkit-animation: flipInX 3s infinite;
+  line-height: 0;
+}
+
+@keyframes flipInX {
+  0% {
+    -webkit-transform: perspective(400px) rotateX(90deg);
+    -ms-transform: perspective(400px) rotateX(90deg);
+    transform: perspective(400px) rotateX(90deg);
+    opacity: 0;
+  }
+
+  40% {
+    -webkit-transform: perspective(400px) rotateX(-20deg);
+    -ms-transform: perspective(400px) rotateX(-20deg);
+    transform: perspective(400px) rotateX(-20deg);
+  }
+
+  55% {
+    -webkit-transform: perspective(400px) rotateX(25deg);
+    -ms-transform: perspective(400px) rotateX(25deg);
+    transform: perspective(400px) rotateX(25deg);
+  }
+
+  80% {
+    -webkit-transform: perspective(400px) rotateX(-30deg);
+    -ms-transform: perspective(400px) rotateX(-30deg);
+    transform: perspective(400px) rotateX(-30deg);
+    opacity: 1;
+  }
+  100% {
+    -webkit-transform: perspective(400px) rotateX(90deg);
+    -ms-transform: perspective(400px) rotateX(90deg);
+    transform: perspective(400px) rotateX(90deg);
+    opacity: 0;
+  }
+}
+</style>
