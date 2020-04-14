f14a485a78c91ac5c4a0401a8548e189335c147a
Allan Chain <allanchain@pku.edu.cn>
2020-04-14 21:20:47 +0800

    Enhance: require css in vuetify plugin file

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import store from './store'
 
 Vue.config.productionTip = false
