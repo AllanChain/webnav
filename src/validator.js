73ff65c287aad4050f84c791f6a2a65d65b1b669
Allan Chain <allanchain@pku.edu.cn>
2020-04-18 15:41:01 +0800

    Fix: remove console

--- src/validator.js
+++ src/validator.js
@@ -16,7 +16,6 @@ export default (schema, payload, quiet) => {
     return true
   if (quiet) return false
   validate.errors.map(e => {
-    console.log(e)
     let message = pointer.get(payload, e.dataPath)
     if (typeof message === 'object')
       message = JSON.stringify(message)
