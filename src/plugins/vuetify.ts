dd634a27d77a8e8b7fd725f2497cdb73b35b212e
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-08-31 16:55:36 +0800

    chore: keep up with vuetify 3 alpha 11

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,5 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/lib/styles/main.sass'
 import { createVuetify } from 'vuetify'
-import * as directives from 'vuetify/lib/directives'
 
-export default createVuetify({
-  directives
-})
+export default createVuetify()
--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ViteComponents from 'vite-plugin-components'
+import vuetify from '@vuetify/vite-plugin'
 import path from 'path'
-
-const srcPath = path.resolve(__dirname, 'src', 'styles', 'variables.sass')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    ViteComponents({
-      globalComponentsDeclaration: true,
-      customComponentResolvers: [
-        (name: string) => {
-          if (name.match(/^V[A-Z]/))
-            return { importName: name, path: 'vuetify/lib/components' }
-        }
-      ]
-    })
+    vuetify({ styles: 'expose' })
   ],
   define: { 'process.env': {} },
-  css: {
-    preprocessorOptions: {
-      sass: { additionalData: `@import ${srcPath}\n` },
-      scss: { additionalData: `@import ${srcPath};\n` }
-    }
-  },
   resolve: {
     alias: {
       '@': path.resolve('./src')
     }
   }
-  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
-  resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ]
-  },
-  */
 })
