25d290aceebebb4fface91a8402328453a0513fb
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-02-04 17:55:37 +0800

    test: remove unstable test without intercept

--- cypress/e2e/simple.cy.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-describe('Default view', () => {
-  it('Looks fine without intercept', () => {
-    cy.visit('/')
-    cy.get('img[data-cy="website-icon"]:not([src*="fallback"])', {
-      timeout: 10000
-    }).should('have.length.above', 0)
-  })
-})
