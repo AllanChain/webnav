f5fcbb8bcc5527dfc3651c0c07fe1daf3bac1390
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-02-04 17:42:56 +0800

    fix: setup PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
-import path from 'path'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'service-worker.js',
+      injectRegister: false,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest,woff2}']
+      }
+    }),
     vuetify({
       styles: { configFile: 'src/settings.scss' }
     }),
