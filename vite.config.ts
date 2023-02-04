8c850d28d31f4a60e0deaeed29451bdca112fdac
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-02-04 18:16:46 +0800

    fix: add web manifest

--- vite.config.ts
+++ vite.config.ts
@@ -15,20 +15,52 @@ export default defineConfig({
       filename: 'service-worker.ts',
       injectRegister: false,
       injectManifest: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest,woff2}']
-      }
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest,woff2}'],
+      },
+      manifest: {
+        name: 'WebNav',
+        short_name: 'WebNav',
+        theme_color: '#4c89fe',
+        icons: [
+          {
+            src: './img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: './img/icons/android-chrome-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: './img/icons/android-chrome-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#4c89fe',
+      },
     }),
     vuetify({
-      styles: { configFile: 'src/settings.scss' }
+      styles: { configFile: 'src/settings.scss' },
     }),
     VueI18nPlugin({
       include: path.resolve(__dirname, './src/locales/**'),
-    })
+    }),
   ],
   define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': path.resolve('./src')
-    }
-  }
+      '@': path.resolve('./src'),
+    },
+  },
 })
