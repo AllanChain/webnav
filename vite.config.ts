93bd1ef05c6c87c7d3934d426a80f0dd25c48732
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-08-03 20:25:55 +0800

    revert: add bundle size visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import { VitePWA } from 'vite-plugin-pwa'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -56,8 +55,7 @@ export default defineConfig({
     }),
     VueI18nPlugin({
       include: path.resolve(__dirname, './src/locales/**')
-    }),
-    process.env.VITE_SHOW_BUNDLE === '' ? null : visualizer()
+    })
   ],
   define: { 'process.env': {} },
   resolve: {
